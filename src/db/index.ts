9c065d35841a37f5d403a1ea5809be676966b95f
Steve Brown <steve.brown+github@brownserve.co.uk>
2026-06-06 00:39:04 +0100

    feat: automatically perform DB migrations

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,7 @@
 import { mkdirSync } from "fs";
 import { dirname, resolve } from "path";
 import { drizzle } from "drizzle-orm/better-sqlite3";
+import { migrate } from "drizzle-orm/better-sqlite3/migrator";
 import Database from "better-sqlite3";
 import * as schema from "./schema";
 
@@ -12,3 +13,5 @@ const dbPath = resolve(
 mkdirSync(dirname(dbPath), { recursive: true });
 
 export const db = drizzle(new Database(dbPath), { schema });
+
+migrate(db, { migrationsFolder: resolve(process.cwd(), "drizzle") });
